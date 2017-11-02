86118b5064189ae80c3f62be04b362ad2d975a86
Nguyenj93 <jnguyen9372@gmail.com>
2017-11-02 17:02:14 -0400

    UI Change

--- app/src/main/java/erau/ecocar3year4/MainActivity.java
+++ app/src/main/java/erau/ecocar3year4/MainActivity.java
@@ -1,13 +1,9 @@
 package erau.ecocar3year4;
 
-
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-
-
-
 
 public class MainActivity extends AppCompatActivity {
 
