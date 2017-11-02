bb6b20a2846083b2ffc7c95febe4c77260ce1630
Nguyenj93 <jnguyen9372@gmail.com>
2017-11-02 16:47:00 -0400

    UI Change.

--- app/src/main/java/erau/ecocar3year4/MainActivity.java
+++ app/src/main/java/erau/ecocar3year4/MainActivity.java
@@ -1,12 +1,11 @@
 package erau.ecocar3year4;
 
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
+
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
+
 
 
 
